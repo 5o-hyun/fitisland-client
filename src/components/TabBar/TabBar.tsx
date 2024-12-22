1bc37968dc49994993298adba9c277f8485860f0
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2024-12-22 19:32:43 +0900

    feat: 상단바 기능추가

--- src/components/TabBar/TabBar.tsx
+++ src/components/TabBar/TabBar.tsx
@@ -4,10 +4,12 @@ import Arrow from 'public/home/arrow.svg';
 
 type TabBarProps = {
   name: string;
+  rightText?: string;
   onClickBack?: () => void;
+  onClickRight?: () => void;
 };
 
-const TabBar = ({ name, onClickBack }: TabBarProps) => {
+const TabBar = ({ name, rightText, onClickBack, onClickRight }: TabBarProps) => {
   return (
     <div className="h-[60px] border-b-[1px] border-solid border-[#404040] flex items-center relative">
       {onClickBack && (
@@ -19,7 +21,14 @@ const TabBar = ({ name, onClickBack }: TabBarProps) => {
         </div>
       )}
       <p className="w-full text-center">{name}</p>
-      <div className="w-[48px] h-[48px] flex justify-center items-center absolute right-0"></div>
+      {rightText && (
+        <div
+          className="w-[48px] h-[48px] flex justify-center items-center absolute right-0 cursor-pointer text-primary"
+          onClick={onClickRight}
+        >
+          {rightText}
+        </div>
+      )}
     </div>
   );
 };
